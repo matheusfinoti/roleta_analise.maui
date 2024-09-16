0a1afa2b885d6d4bfdee1250bccafa15525c7d92
matheusfinoti <matheusfinoti@gmail.com>
2024-09-16 19:57:27 -0300

    Criado classe ManipulaJson, separando reponsabilidade de salvar o código em Json.

---
+++ RoletaAnalise.maui/Context/ManipulaJson.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RoletaAnalise.maui.Models;
+
+namespace RoletaAnalise.maui.Context
+{
+    internal class ManipulaJson
+    {
+        public string _filePath { get; set; }
+
+
+        public ManipulaJson()
+        {
+            _filePath = BuscaCaminho();
+        }
+
+        private string BuscaCaminho()
+        {
+            var caminhoRaizProjeto = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\..\..\..\"));
+            return Path.Combine(caminhoRaizProjeto, "data", "numeros.json");
+        }
+
+        public void AdicionaNumero(string json)
+        {
+            File.WriteAllText(this._filePath, json);
+        }
+
+    }
+}
